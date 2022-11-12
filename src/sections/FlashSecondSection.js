8fc1d3c4e7a49093c00cd825c2e15e783440fade
santaforse <santaforsell@gmail.com>
2022-11-12 23:50:01 +0200

    sdss

--- src/sections/FlashSecondSection.js
+++ src/sections/FlashSecondSection.js
@@ -7,21 +7,33 @@ const FlashFirstSection = ({flashTitle2}) => {
   return (
     <>
         <section className="__flash-sale-right container">
-            <div className="__two-for-left"> 
-                <div className="__two-for-left-grid">
-                    <ProductGridSection items={flashProducts} />
-                </div>
-            </div>
-            <div className="__two-for-right"> 
-                <div className="__two-for-right-box">
-                    <h1>{flashTitle2}</h1>
-                    <a href="#" className="btn-theme-white">
+            <div className="flashSmallScreenSecond">
+                <h1>{flashTitle2}</h1>
+                <a href="#" className="btn-theme-white">
                     <div className="line-button-l-1"></div>
                     <div className="line-button-l-2"></div>
                     <p>FLASH SALE</p>
                     <div className="line-button-r-1"></div>
                     <div className="line-button-r-2"></div>
                     </a>
+            </div>
+            <div className="__two-for-left"> 
+                <div className="__two-for-left-grid">
+                    <ProductGridSection items={flashProducts} />
+                </div>
+            </div>
+            <div className="__two-for-right">
+                <div className="flashBigScreenSecond">
+                    <div className="__two-for-right-box">
+                        <h1>{flashTitle2}</h1>
+                        <a href="#" className="btn-theme-white">
+                        <div className="line-button-l-1"></div>
+                        <div className="line-button-l-2"></div>
+                        <p>FLASH SALE</p>
+                        <div className="line-button-r-1"></div>
+                        <div className="line-button-r-2"></div>
+                        </a>
+                    </div>
                 </div>
             </div>
         </section>
