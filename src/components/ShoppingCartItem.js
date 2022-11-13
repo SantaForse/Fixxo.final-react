9a25d6c75d25b6eba05951472644c415dbf2d141
santaforse <santaforsell@gmail.com>
2022-11-13 21:24:14 +0200

    sdasd

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -11,15 +11,16 @@ const ShoppingCartItem = ({item}) => {
             <div className="item-info">
                 <div className="item-info-name">{item.product.name}</div>
                 <div className="item-info-quantity">
-                    <button onClick={() => incrementQuantity(item)}>+</button>
-                    <span>{item.quantity}</span>
-                    <button onClick={() => decrementQuantity(item)}>-</button>
+                    <div className="item-info-quantity-box">
+                        <button onClick={() => incrementQuantity(item)}>-</button>
+                        <span>{item.quantity}</span>
+                        <button onClick={() => decrementQuantity(item)}>+</button>
+                    </div>
                 </div>
             </div>
             <div className="item-price">
                 <div>{item.product.price * item.quantity}</div>
                 <button onClick={() => removeItem(item.articleNumber)}><i class="fa-solid fa-trash"></i></button>
-
             </div>
         </div>
     )
